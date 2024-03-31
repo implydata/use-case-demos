b8124c4350f6869e90ba93ea42005978013b7959
Hellmar Becker <becker@hellmar-becker.de>
2024-03-31 23:34:32 +0200

    Add event and user satellite tables

--- DreamGames/datagen/datagen.py
+++ DreamGames/datagen/datagen.py
@@ -13,6 +13,8 @@ import sqlite3
 
 fake = Faker()
 GAMEID = ('G1', 'G2', 'G3')
+EVENTTYPE = ('Register', 'Sign In', 'Purchase', 'AdResponse', 'Payment', 'GameProgress', 'Game/device error', 'start Game', 'end Game', 'quit Game')
+ADPREFS = ('sports', 'kids', 'clothes', 'dating', 'crypto')
 
 msgCount = 0
 
@@ -22,9 +24,9 @@ def emit(producer, topic, key, emitRecord):
     global msgCount
     # sid = emitRecord['sid']
     if producer is None:
-        print(f'{key}|{json.dumps(emitRecord)}')
+        print(f'{topic}|{key}|{json.dumps(emitRecord)}')
     else:
-        producer.produce(topic, key=str(key), value=str(emitRecord))
+        producer.produce(topic, key=str(key), value=json.dumps(emitRecord))
         msgCount += 1
         if msgCount >= 2000:
             producer.flush()
@@ -34,6 +36,35 @@ def emit(producer, topic, key, emitRecord):
 def emitEvent(p, t, emitRecord):
     emit(p, t, emitRecord["sessionId"], emitRecord)
 
+def emitEventDetail(p, t, k):
+    evd = {
+        'eventId': k,
+        'eventType': EVENTTYPE[int(k)]
+    }
+    emit(p, t, k, evd)
+
+def emitUserDetail(p, t, k):
+    playerdetail = k.split('-')
+    ud = {
+        'userId': playerdetail[0],
+        'playerId': k,
+        'userName': fake.user_name(),
+        'payingCustomer': fake.random_element(elements=('Y', 'N')),
+        'deviceDetail': {
+            'deviceId': fake.uuid4(),
+            'deviceType': fake.random_element(elements=('mobile', 'desktop')),
+            'deviceOS': fake.random_element(elements=('Linux', 'Windows', 'macOS', 'iOS', 'Android')),
+            'deviceManufacturer': fake.random_element(elements=('Apple', 'Samsung', 'Huawei', 'Xiaomi', 'Dell', 'HP')),
+            'gameVersion': fake.numerify('%.#')
+        },
+        'playerDemographics': {
+            'ageRange': fake.random_element(elements=('18-25', '26-35', '36-50', '51-60', '61+')), 
+            'pronouns': fake.random_element(elements=('he/him', 'she/her', 'they/them')),
+            'genreOfInterest': fake.random_element(elements=('action', 'strategy', 'casual'))
+        },
+        'playerAdPreferences': fake.random_sample(ADPREFS)
+    }
+    emit(p, t, k, ud)
 
 # Read configuration
         
@@ -83,15 +114,26 @@ def main():
 
     if args.dry_run:
         producer = None
-        eventTopic = None
     else:
-        eventTopic = config['General']['eventTopic']
         logging.debug(f'eventTopic: {eventTopic}')
 
         kafkaconf = config['Kafka']
         kafkaconf['client.id'] = socket.gethostname()
         logging.debug(f'Kafka client configuration: {kafkaconf}')
         producer = Producer(kafkaconf)
+
+    try:
+        eventTopic = config['General']['eventTopic']
+        eventDetailTopic = config['General']['eventDetailTopic']
+        userDetailTopic = config['General']['userDetailTopic']
+        gameDetailTopic = config['General']['gameDetailTopic']
+        advertiserDetailTopic = config['General']['advertiserDetailTopic']
+    except KeyError:
+        eventTopic = None    
+        eventDetailTopic = None    
+        userDetailTopic = None    
+        gameDetailTopic = None    
+        advertiserDetailTopic = None    
 
     minSleep = config['General']['minSleep']
     if minSleep is None:
@@ -104,13 +146,16 @@ def main():
 
         tNow = int(time.time())
         
+        eventId = str(fake.random_int(min=0, max=9))
+        userId = fake.numerify('######')
+        playerId = userId + '-' + fake.numerify('###') # convention: playerId is userId-suffix
         ev = {
-            'eventId' : fake.random_int(min=1, max=10),
+            'eventId' : eventId,
             'eventTimestamp' : tNow - fake.random_int(min=1, max=100),
             'timestamp' : tNow,
             'gameId' : fake.random_element(elements=GAMEID),
-            'userId' : fake.user_name(),
-            'playerId' : fake.user_name(),
+            'userId' : userId,
+            'playerId' : playerId,
             'sessionId' : fake.numerify('######'),
             'sessionStatus' : fake.random_element(elements=('start', 'in-progress', 'complete', 'fail')),
             'score' : fake.random_int(min=0, max=100000),
@@ -121,6 +166,8 @@ def main():
             'adResponse' : fake.random_element(elements=('', 'view', 'click'))
         }
         emitEvent(producer, eventTopic, ev)
+        emitEventDetail(producer, eventDetailTopic, eventId)
+        emitUserDetail(producer, userDetailTopic, playerId)
 
         waitSecs = random.uniform(minSleep, maxSleep)
         logging.debug(f'wait time: {waitSecs}')
